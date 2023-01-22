5f350671e10dffbbaa20b592c7541ffb18a53b5d
+ <+>
2023-01-22 01:30:33 -0500

    commit tree ef20023e278a029a0b87f63fe89dca4cefb2a146

--- src/components/About.js
+++ src/components/About.js
@@ -6,11 +6,13 @@ import ArticleList from "./ArticleList";
 
 console.log(blogData);
 
+
+
 function App() {
   return (
     <div className="App">
       <Header name={blogData.name} />
-      <About image={blogData.image} about={blogData.about} />
+      <About image={blogData.image} about={blogData.about}/>
       <ArticleList posts={blogData.posts} />
     </div>
   );
